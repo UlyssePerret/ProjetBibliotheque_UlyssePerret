fc7de5dedd3939c94d5665c08489141a892e2e3e
Ulysse Perret <ulysseperret@orange.fr>
2022-02-09 15:08:44 +0100

    Initial Commit

---
+++ ProjetBibliotheque_UlyssePerret/ProjetBibliotheque_UlyssePerretApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetBibliotheque_UlyssePerretApp.swift
+//  ProjetBibliotheque_UlyssePerret
+//
+//  Created by Ulysse Perret on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjetBibliotheque_UlyssePerretApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
